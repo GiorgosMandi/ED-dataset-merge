d0e24492d0596d4c3084a56e9690f6c6d57ca738
George <giwrgosmandi@gmail.com>
2021-06-26 14:17:02 +0300

    transform-json in EDD

--- src/transformers/EDD_Transformer.py
+++ src/transformers/EDD_Transformer.py
@@ -40,93 +40,99 @@ class EDDTransformer(Transformer):
         utilities.write_iterable(event_paths, events)
 
     def transform(self, output_path):
+        id = 0
+        if os.path.isdir(self.path):
+            for file in tqdm(os.listdir(self.path)):
+                json_file = self.path + file
+                edd_jsons = utilities.read_json(json_file)
+                self.transform_json(edd_jsons, output_path, id)
+                id += 1
+        else:
+            self.transform_json(self.path, output_path, id)
+
+    def transform_json(self, edd_jsons, output_path, id):
         new_instances = []
-        i = -1
-        for file in tqdm(os.listdir(self.path)):
-            json_file = self.path + file
-            edd_jsons = utilities.read_json(json_file)
-            for instance in edd_jsons:
-                instance_data = instance['data']
+        for instance in edd_jsons:
+            instance_data = instance['data']
+            new_instance_id = self.id_base + "-" + instance_data['filename'] + str(id)
+            text_sentence = re.sub(r'(?<!\.)\n', ' . ', instance_data['text']).replace("\n", "")
 
-                new_instance_id = self.id_base + "-" + instance_data['filename'] + str(i)
-                text_sentence = re.sub(r'(?<!\.)\n', ' . ', instance_data['text']).replace("\n", "")
+            sentences = []
+            words = []
+            lemma = []
+            pos_tags = []
+            entity_types = []
+            chunks = []
+            penn_treebanks = []
+            dependency_parsing = []
+            for sentence in filter(None, text_sentence.strip().split(".", )):
+                parsing = self.advanced_parsing(sentence)
+                words.extend(parsing['words'])
+                pos_tags.extend(parsing['pos-tag'])
+                lemma.extend(parsing['lemma'])
+                entity_types.extend(parsing['ner'])
+                sentences.extend(parsing['sentences'])
+                penn_treebanks.extend(parsing['treebank'])
+                dependency_parsing.extend(parsing['dep-parse'])
+                chunks.extend(parsing['chunks'])
 
-                sentences = []
-                words = []
-                lemma = []
-                pos_tags = []
-                entity_types = []
-                chunks = []
-                penn_treebanks = []
-                dependency_parsing = []
-                for sentence in filter(None, text_sentence.strip().split(".", )):
-                    parsing = self.advanced_parsing(sentence)
-                    words.extend(parsing['words'])
-                    pos_tags.extend(parsing['pos-tag'])
-                    lemma.extend(parsing['lemma'])
-                    entity_types.extend(parsing['ner'])
-                    sentences.extend(parsing['sentences'])
-                    penn_treebanks.extend(parsing['treebank'])
-                    dependency_parsing.extend(parsing['dep-parse'])
-                    chunks.extend(parsing['chunks'])
+            no_of_sentences = len(sentences)
 
-                no_of_sentences = len(sentences)
-
-                event_type = ""
-                trigger = {}
-                arguments = []
-                entities = []
-                instance_result = instance['completions'][0]['result']
-                for i, result in enumerate(instance_result):
-                    if result['from_name'] == "ev_type":
-                        event_type = result['value']['choices'][0].lower()
-                        event_type = self.events_mapper[event_type]
+            event_type = ""
+            trigger = {}
+            arguments = []
+            entities = []
+            instance_result = instance['completions'][0]['result']
+            for i, result in enumerate(instance_result):
+                if result['from_name'] == "ev_type":
+                    event_type = result['value']['choices'][0].lower()
+                    event_type = self.events_mapper[event_type]
+                else:
+                    value = result['value']
+                    entity_start = value['start']
+                    entity_end = value['end']
+                    entity_text = value['text']
+                    role = value['labels'][0].lower()
+                    if role == 'event trigger':
+                        trigger = {'start': entity_start, 'end': entity_end, 'text': entity_text}
                     else:
-                        value = result['value']
-                        # WARNING: start and end do not point to the exact word
-                        entity_start = value['start']
-                        entity_end = value['end']
-                        entity_text = value['text']
-                        role = value['labels'][0].lower()
-                        if role == 'event trigger':
-                            trigger = {'start': entity_start, 'end': entity_end, 'text': entity_text}
+                        entity_id = new_instance_id + "-" + str(i)
+                        types = set(entity_types[entity_start: entity_end])
+                        entity_type = "O"
+                        if len(types) > 0:
+                            entity_type = utilities.most_frequent(entity_types[entity_start: entity_end])
+
+                        entity = {'start': entity_start, 'end': entity_end, 'text': entity_text, 'entity-id': entity_id,
+                                  'entity-type': entity_type, 'existing-entity-type': ""}
+                        argument = entity.copy()
+                        if role in self.roles_mapper:
+                            role = self.roles_mapper[role]
+                            argument['role'] = role
+                            entities.append(entity)
+                            arguments.append(argument)
                         else:
-                            entity_id = new_instance_id + "-" + str(i)
-                            types = set(entity_types[entity_start: entity_end])
-                            entity_type = "O"
-                            if len(types) > 0:
-                                entity_type = utilities.most_frequent(entity_types[entity_start: entity_end])
+                            print(role)
 
-                            entity = {'start': entity_start, 'end': entity_end, 'text': entity_text, 'entity-id': entity_id,
-                                      'entity-type': entity_type, 'existing-entity-type': ""}
-                            argument = entity.copy()
-                            if role in self.roles_mapper:
-                                role = self.roles_mapper[role]
-                                argument['role'] = role
-                                entities.append(entity)
-                                arguments.append(argument)
-                            else:
-                                print(role)
-
-                events_triples = {'arguments': arguments, 'trigger': trigger, 'event-type': event_type}
-                new_instance = {
-                    'origin': self.origin,
-                    'id': new_instance_id,
-                    'no-of-sentences': no_of_sentences,
-                    'sentences': sentences,
-                    'text': text_sentence,
-                    'words': words,
-                    'lemma': lemma,
-                    'pos-tags': pos_tags,
-                    'ner': entity_types,
-                    'golden-entity-mentions': entities,
-                    'golden-event-mentions': events_triples,
-                    'penn-treebank': penn_treebanks,
-                    'dependency-parsing': dependency_parsing,
-                    'chunks': chunks
-                }
-                new_instances.append(new_instance)
-                if len(new_instances) == self.batch_size:
-                    utilities.write_json(new_instances, output_path)
-                    new_instances = []
+            events_triples = {'arguments': arguments, 'trigger': trigger, 'event-type': event_type}
+            new_instance = {
+                'origin': self.origin,
+                'id': new_instance_id,
+                'no-of-sentences': no_of_sentences,
+                'sentences': sentences,
+                'text': text_sentence,
+                'words': words,
+                'lemma': lemma,
+                'pos-tags': pos_tags,
+                'ner': entity_types,
+                'golden-entity-mentions': entities,
+                'golden-event-mentions': events_triples,
+                'penn-treebank': penn_treebanks,
+                'dependency-parsing': dependency_parsing,
+                'chunks': chunks
+            }
+            new_instances.append(new_instance)
+            if len(new_instances) == self.batch_size:
+                utilities.write_json(new_instances, output_path)
+                new_instances = []
         utilities.write_json(new_instances, output_path)
+
