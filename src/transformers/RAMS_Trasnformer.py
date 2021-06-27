7db236c756108e6f67ef2ce77e6b720d55457bc9
George <giwrgosmandi@gmail.com>
2021-06-27 15:15:57 +0300

    RAMS all event types & roles

--- src/transformers/RAMS_Trasnformer.py
+++ src/transformers/RAMS_Trasnformer.py
@@ -3,6 +3,7 @@ from .Transformer import Transformer
 from tqdm import tqdm
 from ..utils import utilities
 import time
+import json
 
 
 class RamsTransformer(Transformer):
@@ -14,105 +15,119 @@ class RamsTransformer(Transformer):
         self.rams_path = rams_path
         self.origin = "RAMS"
 
+    def export_types(self):
+        events = set()
+        roles = set()
+        with open(self.rams_path) as json_file:
+            for inline_json in tqdm(json_file):
+                instance = json.loads(inline_json)
+                for triple in instance['gold_evt_links']:
+                    event = instance['evt_triggers'][0][2][0][0]
+                    if event not in self.events_mapper.keys():
+                        events.add(event)
+                    role = re.split("\d", triple[2])[-1]
+                    if role not in self.roles_mapper.keys():
+                        roles.add(role)
+        return events, roles
+
     # transform dataset to the common schema
     def transform(self, output_path):
         self.log.info("Starts transformation of RAMS")
         start_time = time.monotonic()
-
         new_instances = []
         i = -1
-        rams_jsons = utilities.read_jsonlines(self.rams_path)
-        for instance in tqdm(rams_jsons):
-            i += 1
-            new_instance_id = self.id_base + str(i) + "-" + instance['doc_key']
-            # parsing sentences
-            text_sentences = " ".join([t for s in instance['sentences'] for t in s])
-            sentences = []
-            words = []
-            lemma = []
-            pos_tags = []
-            entity_types = []
-            chunks = []
-            penn_treebanks = []
-            dependency_parsing = []
-            for sentence in instance['sentences']:
-                sentence = ' '.join(sentence)
-                parsing = self.advanced_parsing(sentence)
-                words.extend(parsing['words'])
-                lemma.extend(parsing['lemma'])
-                pos_tags.extend(parsing['pos-tag'])
-                entity_types.extend(parsing['ner'])
-                sentences.extend(parsing['sentences'])
+        with open(self.rams_path) as json_file:
+            for inline_json in tqdm(json_file):
+                instance = json.loads(inline_json)
 
-                # sentence centric
-                penn_treebanks.extend(parsing['treebank'])
-                dependency_parsing.extend(parsing['dep-parse'])
-                chunks.extend(parsing['chunks'])
+                i += 1
+                new_instance_id = self.id_base + str(i) + "-" + instance['doc_key']
+                # parsing sentences
+                text_sentences = " ".join([t for s in instance['sentences'] for t in s])
+                sentences = []
+                words = []
+                lemma = []
+                pos_tags = []
+                entity_types = []
+                chunks = []
+                penn_treebanks = []
+                dependency_parsing = []
+                for sentence in instance['sentences']:
+                    sentence = ' '.join(sentence)
+                    parsing = self.advanced_parsing(sentence)
+                    words.extend(parsing['words'])
+                    lemma.extend(parsing['lemma'])
+                    pos_tags.extend(parsing['pos-tag'])
+                    entity_types.extend(parsing['ner'])
+                    sentences.extend(parsing['sentences'])
 
-            no_of_sentences = len(sentences)
+                    # sentence centric
+                    penn_treebanks.extend(parsing['treebank'])
+                    dependency_parsing.extend(parsing['dep-parse'])
+                    chunks.extend(parsing['chunks'])
 
-            # process entities
-            entities = []
-            for j, entity in enumerate(instance['ent_spans']):
-                start = entity[0]
-                end = entity[1] + 1
-                text = ' '.join(words[start: end])
-                entity_id = new_instance_id + "-entity-" + str(j)
+                no_of_sentences = len(sentences)
 
-                # multiple words may result to multiple types - pick the most frequent type
-                entity_type = utilities.most_frequent(entity_types[start: end])
-                new_entity = {'start': start, 'end': end, 'text': text, 'entity-id': entity_id,
-                              'entity-type': entity_type, 'existing-entity-type': ""}
-                entities.append(new_entity)
+                # process entities
+                entities = []
+                for j, entity in enumerate(instance['ent_spans']):
+                    start = entity[0]
+                    end = entity[1] + 1
+                    text = ' '.join(words[start: end])
+                    entity_id = new_instance_id + "-entity-" + str(j)
 
-            # process trigger
-            if len(instance['evt_triggers']) > 1:
-                print("ERROR: More triggers than expected")
+                    # multiple words may result to multiple types - pick the most frequent type
+                    entity_type = utilities.most_frequent(entity_types[start: end])
+                    new_entity = {'start': start, 'end': end, 'text': text, 'entity-id': entity_id,
+                                  'entity-type': entity_type, 'existing-entity-type': ""}
+                    entities.append(new_entity)
 
-            trigger_start = instance['evt_triggers'][0][0]
-            trigger_end = instance['evt_triggers'][0][1] + 1
-            trigger_text = ' '.join(words[trigger_start: trigger_end]).lower()
-            trigger = {'start': trigger_start, 'end': trigger_end, 'text': trigger_text}
+                # process trigger
+                if len(instance['evt_triggers']) > 1:
+                    print("ERROR: More triggers than expected")
 
-            # process events - construct event-triples
-            event_type = instance['evt_triggers'][0][2][0][0]
-            event_type = self.events_mapper[event_type]
-            events_triples = []
-            arguments = []
-            for triple in instance['gold_evt_links']:
-                arg_start = triple[1][0]
-                arg_end = triple[1][1] + 1
-                text = ' '.join(words[arg_start: arg_end])
-                entity_type = utilities.most_frequent(entity_types[arg_start: arg_end])
-                arg_role = re.split("\d", triple[2])[-1]
-                arg_role = self.roles_mapper[arg_role]
-                argument = {'start': arg_start, 'end': arg_end, 'text': text, 'role': arg_role,
-                            'entity-type': entity_type, 'existing-entity-type': ""}
-                arguments.append(argument)
+                trigger_start = instance['evt_triggers'][0][0]
+                trigger_end = instance['evt_triggers'][0][1] + 1
+                trigger_text = ' '.join(words[trigger_start: trigger_end]).lower()
+                trigger = {'start': trigger_start, 'end': trigger_end, 'text': trigger_text}
 
-            events_triples.append({'arguments': arguments, 'trigger': trigger, 'event-type': event_type})
+                # process events - construct event-triples
+                event_type = instance['evt_triggers'][0][2][0][0]
+                event_type = self.events_mapper[event_type]
+                events_triples = []
+                arguments = []
+                for triple in instance['gold_evt_links']:
+                    arg_start = triple[1][0]
+                    arg_end = triple[1][1] + 1
+                    text = ' '.join(words[arg_start: arg_end])
+                    entity_type = utilities.most_frequent(entity_types[arg_start: arg_end])
+                    arg_role = re.split("\d", triple[2])[-1]
+                    arg_role = self.roles_mapper[arg_role]
+                    argument = {'start': arg_start, 'end': arg_end, 'text': text, 'role': arg_role,
+                                'entity-type': entity_type, 'existing-entity-type': ""}
+                    arguments.append(argument)
 
-            new_instance = {
-                'origin': self.origin,
-                'id': new_instance_id,
-                'no-of-sentences': no_of_sentences,
-                'sentences': sentences,
-                'text': text_sentences,
-                'words': words,
-                'lemma': lemma,
-                'pos-tags': pos_tags,
-                'ner': entity_types,
-                'golden-entity-mentions': entities,
-                'golden-event-mentions': events_triples,
-                'penn-treebank': penn_treebanks,
-                "dependency-parsing": dependency_parsing,
-                'chunks': chunks
-            }
-            new_instances.append(new_instance)
-            if len(new_instances) == self.batch_size:
-                utilities.write_json(new_instances, output_path)
-                new_instances = []
+                events_triples.append({'arguments': arguments, 'trigger': trigger, 'event-type': event_type})
+
+                new_instance = {
+                    'origin': self.origin,
+                    'id': new_instance_id,
+                    'no-of-sentences': no_of_sentences,
+                    'sentences': sentences,
+                    'text': text_sentences,
+                    'words': words,
+                    'lemma': lemma,
+                    'pos-tags': pos_tags,
+                    'ner': entity_types,
+                    'golden-entity-mentions': entities,
+                    'golden-event-mentions': events_triples,
+                    'penn-treebank': penn_treebanks,
+                    "dependency-parsing": dependency_parsing,
+                    'chunks': chunks
+                }
+                new_instances.append(new_instance)
+                if len(new_instances) == self.batch_size:
+                    utilities.write_json(new_instances, output_path)
+                    new_instances = []
         utilities.write_json(new_instances, output_path)
         self.log.info("Transformation of RAMS completed in " + str(round(time.monotonic() - start_time, 3)) + "sec")
-
-
